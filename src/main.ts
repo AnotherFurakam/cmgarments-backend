f986c5bf8066e2dbefdeb26f716ce61e40c2b1e4
Wilmer <wilmer.r2004@gmail.com>
2023-02-25 02:43:04 -0500

    agregando los cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
 
   //*CONFIGURACIÓN DEL VALIDATON
   //La configuración de whitelist: true, nos permite que las propiedades que no son validadas
