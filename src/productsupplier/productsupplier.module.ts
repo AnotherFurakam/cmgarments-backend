a36154d35d406cdf88121acadf32f70e9a4c3b3b
AngelC614 <anjocal844@gmail.com>
2023-03-18 02:40:55 -0500

    new

--- src/productsupplier/productsupplier.module.ts
+++ src/productsupplier/productsupplier.module.ts
@@ -4,10 +4,13 @@ import { BrandController } from "src/brand/brand.controller";
 import { BrandService } from "src/brand/brand.service";
 import { CategoryController } from "src/category/category.controller";
 import { CategoryService } from "src/category/category.service";
+import { CloudinaryModule } from "src/cloudinary/cloudinary.module";
 import { Brand } from "src/model/brand.entity";
 import { Category } from "src/model/category.entity";
+import { Image } from "src/model/image.entity";
 import { Product } from "src/model/product.entity";
 import { ProductSupplier } from "src/model/productsupplier.entity";
+import { Purchase_detail } from "src/model/purchase_detail.entity";
 import { Supplier } from "src/model/supplier.entity";
 import { ProductController } from "src/product/product.controller";
 import { ProductService } from "src/product/product.service";
@@ -18,8 +21,19 @@ import { ProductSupplierController } from "./productsupplier.controller";
 import { ProductSupplierService } from "./productsupplier.service";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([ProductSupplier]), TypeOrmModule.forFeature([Product]), TypeOrmModule.forFeature([Supplier]), TypeOrmModule.forFeature([Brand]), TypeOrmModule.forFeature([Category])],
-    providers: [ProductSupplierService, ProductService, SupplierService, BrandService, CategoryService, Repository],
-    controllers: [ProductSupplierController, ProductController, SupplierController, BrandController, CategoryController]
+    imports: [
+        TypeOrmModule.forFeature([
+          ProductSupplier,
+          Product,
+          Supplier,
+          Brand,
+          Category,
+          Purchase_detail,
+          Image,
+        ]),
+        CloudinaryModule,
+      ],    providers: [ProductSupplierService, ProductService, SupplierService, BrandService, CategoryService, Repository],
+    controllers: [ProductSupplierController, ProductController, SupplierController, BrandController, CategoryController],
+    
 })
 export class ProductSupplierModule{}
